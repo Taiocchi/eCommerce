86484f5e558e28908531ef9706086416e39f868e
Luca Taiocchi <Taiocchi>
2024-11-11 13:53:51 +0100

    Versione 01

---
+++ eCommerce/eCommerce/Carrello.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce
+{
+    internal class Carrello
+    {
+        private string identificativo;
+
+        private List<Prodotto> prodottiCarrello = new List<prodotto>();
+
+        public List<Prodotto> ProdottiCarrello
+        {
+            get { return prodottiCarrello; }
+        }
+        public string Identificativo
+        {
+            get { return identificativo; }
+        }
+        public void aggiungiProdotto(Prodotto prodotto)
+        {
+            prodottiCarrello.Add(prodotto);
+        }
+        public void rimuoviProdotto(Prodotto prodotto)
+        {
+            prodottiCarrello.Remove(prodotto);
+        }
+        public void svuotaCarrello()
+        {
+            prodottiCarrello.Clear();
+        }
+    }
+}
---
+++ eCommerce/eCommerce/Prodotto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce
+{
+    internal class Prodotto
+    {
+        private string marca;
+        private string modello;
+        private double prezzo;
+        private string identificativo;
+
+        public string Marca
+        {
+            get { return marca; }
+        }
+
+    }
+}
